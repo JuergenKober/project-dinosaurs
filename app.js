828fe2c891a0cd97e6286054ba665cf98dd6a309
Jürgen Kober <mindeleven@posteo.de>
2021-08-05 13:10:09 +0200

    Added event listener

--- app.js
+++ app.js
@@ -1,3 +1,5 @@
+const form = document.getElementById("dino-compare");
+const button = document.getElementById("btn");
 
     // Create Dino Constructor
 
@@ -5,25 +7,30 @@
     // Create Dino Objects
 
 
-    // Create Human Object
+// Create Human Object
 
-    // Use IIFE to get human data from form
+button.addEventListener("click", (event) {
+  const name = document.getElementById("name").value;
+  console.log(name);
 
+});
+
+   // Use IIFE to get human data from form
 
     // Create Dino Compare Method 1
-    // NOTE: Weight in JSON file is in lbs, height in inches. 
+    // NOTE: Weight in JSON file is in lbs, height in inches.
 
-    
+
     // Create Dino Compare Method 2
     // NOTE: Weight in JSON file is in lbs, height in inches.
 
-    
+
     // Create Dino Compare Method 3
     // NOTE: Weight in JSON file is in lbs, height in inches.
 
 
     // Generate Tiles for each Dino in Array
-  
+
         // Add tiles to DOM
 
     // Remove form from screen
